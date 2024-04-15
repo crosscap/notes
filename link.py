24c4452de20def76ab7391a532c274ad5d22a0fe
crosscap bell <wangliankun2018@outlook.com>
2024-04-15 23:31:23 +0800

    finish chap06 of playing DS

---
+++ link.py
@@ -0,0 +1,54 @@
+class Link:
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_repr = ',' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.rest)
+            self = self.rest
+        return string + str(self.first) + '>'
+
+def range_link(start, end):
+    """Return a Link containing consecutive integers from start to end.
+
+    >>> range_link(3, 6)
+    Link(3, Link(4, Link(5)))
+    """
+    if start >= end:
+        return Link.empty
+    else:
+        return Link(start, range_link(start+1, end))
+
+
+def map_link(f, s):
+    """Return a Link that contains f(x) for each x in Link s.
+
+    >>> map_link(square, range_link(3, 6))
+    Link(9, Link(16, Link(25)))
+    """
+    if s == Link.empty:
+        return Link.empty
+    else:
+        return Link(f(s.first), map_link(f, s.rest))
+
+def filter_link(f, s):
+    """Return a Link that contains only the elements x of Link s for which f(x)
+    is a true value.
+
+    >>> filter_link(odd, range_link(3, 6))
+    Link(3, Link(5))
+    """
+
+square, odd = lambda x: x * x, lambda x : x % 2 == 1
